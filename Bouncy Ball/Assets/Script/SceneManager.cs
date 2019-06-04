e9215a82a6b21230bbd9d9130996930f338ec2fc
kimmin-gi <40197230+kimmin-gi@users.noreply.github.com>
2019-06-04 19:44:03 +0900

    씬 스크립트 수정

--- Bouncy Ball/Assets/Script/SceneManager.cs
+++ Bouncy Ball/Assets/Script/SceneManager.cs
@@ -5,42 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    // GameObject findStar = null;
-    // GameObject findBall = null;
-    // private static int level = 0;
+    GameObject findStar = null;
+    GameObject findBall = null;
+    private static int level = 0;
 
-    // public void changeScene()
-    // {
-    //     //Level의 값을 1올린다.
-    //     level++;
-    //     //다음 스테이지를 로드
-    // }
+    public void changeScene()
+    {
+        //Level의 값을 1올린다.
+        level++;
+        //다음 스테이지를 로드
+    }
 
-    // public void reLoadScene()
-    // {
-    //     //다음 스테이지를 로드
-    //     Application.LoadLevel(level);
-    //     //Level의 값을 1올린다.
-    // }
+    public void reLoadScene()
+    {
+        //다음 스테이지를 로드
+        Application.LoadLevel(level);
+        //Level의 값을 1올린다.
+    }
 
-    // void Update ()
-    // {
-    //     //Tag가 "STAR"인 가장 가까운 물체를 찾아 FindStar에 넣는다.
-    //     //없다면 그 다음 가까운 것을 찾아 넣는다.
-    //     findStar = GameObject.FindWithTag("STAR");
-    //     findBall = GameObject.FindWithTag("BALL");
+    void Update ()
+    {
+        //Tag가 "STAR"인 가장 가까운 물체를 찾아 FindStar에 넣는다.
+        //없다면 그 다음 가까운 것을 찾아 넣는다.
+        findStar = GameObject.FindWithTag("STAR");
+        findBall = GameObject.FindWithTag("BALL");
 
-    //     //만약 null이라면
-    //     if(findStar == null) 
-    //     {
-    //         changeScene();
-    //     }
+        //만약 null이라면
+        if(findStar == null) 
+        {
+            changeScene();
+        }
 
-    //     if (findBall == null)
-    //     {
-    //         reLoadScene();
-    //     }
-    // }
+        if (findBall == null)
+        {
+            reLoadScene();
+        }
+    }
 
     public void firstScene()
     {
